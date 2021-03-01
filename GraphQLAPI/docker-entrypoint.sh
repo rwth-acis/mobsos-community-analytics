30f8186e42792a4893b8c74b4dabd8591683de05
lakhoune <38857665+lakhoune@users.noreply.github.com>
2021-03-01 08:52:40 +0100

    fix port

--- GraphQLAPI/docker-entrypoint.sh
+++ GraphQLAPI/docker-entrypoint.sh
@@ -74,7 +74,7 @@ function selectMnemonic {
 }
 
 #prepare pastry properties
-echo external_address = $(curl -s https://ipinfo.io/ip):${LAS2PEER_PORT} > etc/pastry.properties
+echo external_address = $(curl -s https://ipinfo.io/ip):${GRAPHQL_PORT} > etc/pastry.properties
 
 # start the service within a las2peer node
 if [[ -z "${@}" ]]
